392e71f68b0f509c7a54f43c6bb86d7685a778c8
JulissaDantes <julissa@singularsound.com>
2021-08-03 21:29:30 -0400

    adding tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,13 +1,14 @@
 const { assert } = require("chai");
 
 describe("Contract", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  it("Should return the new x once it's changed", async function () {
     const Contract = await ethers.getContractFactory("Contract");
     const contract = await Contract.deploy();
     await contract.deployed();
-
+    
+    await contract.updateX(15);
     const newx = await contract.x();
-    console.log(newX);
+    assert(newx,15);
     
   });
 });
